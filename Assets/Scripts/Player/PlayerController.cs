2c099cf4f950a2de874581dae9f1e29e5507e35d
William PEPIN <williamp10022000@gmail.com>
2020-05-19 15:14:53 +0200

    added player and camera follower

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D body;
+    [SerializeField] Camera Cam;
+    Vector2 mousePosition;
+    Vector2 direction;
+    [SerializeField]float speed;
+
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(direction.x * speed* Time.fixedDeltaTime, direction.y * speed* Time.fixedDeltaTime);
+        Vector2 lookDirection = mousePosition - body.position;
+        float angle = Mathf.Atan2(lookDirection.y,lookDirection.x)*Mathf.Rad2Deg;
+        body.rotation = angle;
+    }
+    
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical")*speed);
+        mousePosition = Cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
