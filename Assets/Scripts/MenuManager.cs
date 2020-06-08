e8e7138f7b0d721eab73b4ec0668d078630daca3
L <eleonora.dups@gmail.com>
2020-06-08 23:52:29 +0200

    added menu + script menu manager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadInputsScene()
+    {
+        SceneManager.LoadScene("Inputs");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
