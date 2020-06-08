950eedd361f04fae98f5cf8313400e839d1749a9
L <eleonora.dups@gmail.com>
2020-06-08 22:39:03 +0200

    added sprites in board for bsp, animations + started menu

---
+++ Assets/Scripts/AnimationMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D body;
+    private Vector2 movement;
+
+    private Animator animator;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        body.MovePosition(body.position + movement * moveSpeed * Time.deltaTime);
+    }
+}
