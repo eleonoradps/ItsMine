36a6768093359e01da4de30ff26029180d524423
William PEPIN <williamp10022000@gmail.com>
2020-06-09 10:29:28 +0200

    player can put boxes in truck

--- Assets/Scripts/Player/PlayerTakeBoxes.cs
+++ Assets/Scripts/Player/PlayerTakeBoxes.cs
@@ -7,15 +7,16 @@ public class PlayerTakeBoxes : MonoBehaviour
 {
     bool haveBox = false;
     bool canPick = false;
+    bool canPutInTruck = false;
     
     Collider2D boxCollider;
     Spawner spawner;
-    AIStateMachine aiStateMachine;
+    //AIStateMachine aiStateMachine;
 
     void Awake()
     {
         spawner = FindObjectOfType<Spawner>();
-        aiStateMachine = FindObjectOfType<AIStateMachine>();
+        //aiStateMachine = FindObjectOfType<AIStateMachine>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -25,12 +26,20 @@ public class PlayerTakeBoxes : MonoBehaviour
             canPick = true;
             boxCollider = other;
         }
+
+        if (other.CompareTag("playerTruck") && haveBox)
+            canPutInTruck = true;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("box"))
             canPick = false;
+        
+        if (other.CompareTag("playerTruck"))
+        {
+            canPutInTruck = false;
+        }
     }
 
     void Update()
@@ -48,6 +57,12 @@ public class PlayerTakeBoxes : MonoBehaviour
             Destroy(boxCollider.gameObject);
             haveBox = true;
             canPick = false;
+        }
+        
+        if (Input.GetKeyDown("e") && canPutInTruck)
+        {
+            haveBox = false;
+            canPutInTruck = false;
         }
     }
 }
